ab7b81b5c7fbe9648e0167646e61769ec2129949
uno9 <ws060m@gmail.com>
2021-01-11 23:58:20 +0900

    add

--- svelte-todo/src/App.svelte
+++ svelte-todo/src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
+  import Home from './Home';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Home></Home>
 </main>
 
 <style>
@@ -13,18 +12,5 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
--- svelte-todo/src/main.js
+++ svelte-todo/src/main.js
@@ -1,10 +1,14 @@
 import App from './App.svelte';
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
 
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
-	}
+		name: 'svelte'
+  }
+
 });
 
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
 export default app;
